55be54a50168870823e183fd8ee0240f77ef42cf
Patrick Mullaney <patrickwmullaney@gmail.com>
2013-04-14 16:42:28 -0500

    not finished

--- Splay/src/com/algorithms/splay/RedBlack.java
+++ Splay/src/com/algorithms/splay/RedBlack.java
@@ -6,21 +6,99 @@ public class RedBlack {
 	final boolean RED = false;
 	
 	public RedBlack() {
-		root = new RedBlackNode(BLACK);
+		root = null;
 	}
 	
-	public void add(int value) {
-		
+	public void insert(int value) {		
+		// Case 1
+//		if (root == null) {
+//			root = new RedBlackNode(BLACK);
+//			root.setValue(value);
+//			return;
+//		}
+			
+		// insert node as red
 		RedBlackNode tmp = search(value);
 		if (value < tmp.getValue()) {
 			assert(tmp.getLeft() == null);
-			tmp.setLeft(new Node(value));
+			tmp.setLeft(new RedBlackNode(RED));
 		}
 		else {
 			assert(tmp.getRight() == null);
-			tmp.setRight(new Node(value));
+			tmp.setRight(new RedBlackNode(RED));
 		}
+		tmp.setValue(value);
+		
+		insert_case2(tmp);
 		// set color
+	}
+	
+	void insert_case1(RedBlackNode n) {
+		if (n.getParent().getColor() == BLACK)
+			n.setBlack();
+		else
+			insert_case2(n);
+	}
+	
+	void insert_case2(RedBlackNode n) {
+		if (n.getParent().getColor() == BLACK)
+			return;
+		else
+			insert_case3(n);
+	}
+	
+	void insert_case3(RedBlackNode n) {
+		RedBlackNode u = uncle(n);
+		RedBlackNode g;
+		if ((u != null) && (u.getColor() == RED)) {
+			n.getParent().setBlack();
+			g = grandparent(n);
+			g.setRed();
+			insert_case1(g);
+		}
+		else
+			insert_case4(n);
+	}
+	
+	void insert_case4(RedBlackNode n) {
+		RedBlackNode g = grandparent(n);
+		if (n == n.getParent().getRight() && n.getParent() == g.getLeft()) {
+			rotate_left(n.getParent())
+			n = n.getLeft();
+		}
+		else if (n == n.getParent().getLeft() && n.getParent() == g.getRight()) {
+			rotate_right(n.getParent());
+			n = n.getRight();
+		}
+		insert_case5(n);
+	}
+	
+	void insert_case5(RedBlackNode n) {
+		RedBlackNode g = grandparent(n);
+		n.getParent().setBlack();
+		g.setRed();
+		if (n == n.getParent().getLeft())
+			rotate_right(g);
+		else
+			rotate_left(g);
+		
+	}
+	
+	RedBlackNode uncle(RedBlackNode n) {
+		RedBlackNode g = grandparent(n);
+		if (g == null)
+			return g;
+		if (n.getParent() == g.getLeft())
+			return g.getRight();
+		else
+			return g.getLeft();
+	}
+	
+	RedBlackNode grandparent(RedBlackNode n) {
+		if ((n != null) && (n.getParent() != null))
+			return n.getParent().getParent();
+		else 
+			return null;
 	}
 	
 	private RedBlackNode search(int value) {
--- Splay/src/com/algorithms/splay/RedBlackNode.java
+++ Splay/src/com/algorithms/splay/RedBlackNode.java
@@ -1,7 +1,9 @@
 package com.algorithms.splay;
 
-public class RedBlackNode extends Node {
+public class RedBlackNode {
 	boolean black;
+	private int value;
+	private RedBlackNode parent, leftChild, rightChild;
 	
 	public RedBlackNode(boolean b) {
 		super();
@@ -16,4 +18,50 @@ public class RedBlackNode extends Node {
 		black = false;
 	}
 
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public RedBlackNode getParent() {
+		return parent;
+	}
+
+	public void setParent(RedBlackNode parent) {
+		this.parent = parent;
+	}
+
+	public RedBlackNode getLeft() {
+		return leftChild;
+	}
+
+	public void setLeft(RedBlackNode left) {
+		this.leftChild = left;
+		if(left != null) {
+			left.setParent(this);
+		}
+	}
+
+	public RedBlackNode getRight() {
+		return rightChild;
+	}
+
+	public void setRight(RedBlackNode right) {
+		this.rightChild = right;
+		if(right != null) {
+			right.setParent(this);
+		}
+	}
+	
+	public boolean isRoot() {
+		return parent == null;
+	}
+	
+	public boolean getColor() {
+		return black;
+	}
+	
 }
