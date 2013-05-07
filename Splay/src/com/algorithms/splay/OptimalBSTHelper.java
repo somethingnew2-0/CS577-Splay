0d91e97955e61ee0038ae6e51847d91dc79dd1c5
Peter Collins <pmcollins2@wisc.edu>
2013-05-06 22:21:07 -0500

    Simple test of algorithm

--- Splay/src/com/algorithms/splay/OptimalBSTHelper.java
+++ Splay/src/com/algorithms/splay/OptimalBSTHelper.java
@@ -3,25 +3,25 @@ package com.algorithms.splay;
 public class OptimalBSTHelper {
 	/* A Dynamic Programming based function that calculates minimum cost of
 	   a Binary Search Tree. */
-	public static int optimalSearchTree(int keys[], int freq[], int n)
+	public static int optimalSearchTree(int keys[], int freq[])
 	{
 	    /* Create an auxiliary 2D matrix to store results of subproblems */
-	    int cost[][] = new int[n][n];
+	    int cost[][] = new int[keys.length][keys.length];
 	 
 	    /* cost[i][j] = Optimal cost of binary search tree that can be
 	       formed from keys[i] to keys[j].
 	       cost[0][n-1] will store the resultant cost */
 	 
 	    // For a single key, cost is equal to frequency of the key
-	    for (int i = 0; i < n; i++)
+	    for (int i = 0; i < keys.length; i++)
 	        cost[i][i] = freq[i];
 	 
 	    // Now we need to consider chains of length 2, 3, ... .
 	    // L is chain length.
-	    for (int L=2; L<=n; L++)
+	    for (int L=2; L<=keys.length; L++)
 	    {
 	        // i is row number in cost[][]
-	        for (int i=0; i<=n-L+1; i++)
+	        for (int i=0; i<=keys.length-L; i++)
 	        {
 	            // Get column number j from row number i and chain length L
 	            int j = i+L-1;
@@ -39,15 +39,23 @@ public class OptimalBSTHelper {
 	            }
 	        }
 	    }
-	    return cost[0][n-1];
+	    return cost[0][keys.length-1];
 	}
 	 
 	// A utility function to get sum of array elements freq[i] to freq[j]
 	private static int sum(int freq[], int i, int j)
 	{
 	    int s = 0;
-	    for (int k = i; k <=j; k++)
-	       s += freq[k];
+	    for (int k = i; k <=j; k++) {
+	    	System.out.println(i + " " + j);
+	    	s += freq[k];
+	    }
 	    return s;
+	}
+	
+	public static void main(String[] args) {
+		int keys[] = {10, 12, 20};
+	    int freq[] = {34, 8, 50};
+	    System.out.println("Cost of Optimal BST is " + optimalSearchTree(keys, freq));
 	}
 }
