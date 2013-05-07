23f145648450dd60bc783f983b86c4e66c32e7ff
Peter Collins <pmcollins2@wisc.edu>
2013-05-07 16:32:20 -0500

    Add some calculated data with random sampling

--- Splay/src/com/algorithms/splay/TreeComparator.java
+++ Splay/src/com/algorithms/splay/TreeComparator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class TreeComparator {
 	public static final int N = 250; 
-	
+	public static final int SAMPLES = 30;
 	
 	/**
 	 * @param args
@@ -44,27 +44,35 @@ public class TreeComparator {
 		}
 		
 		// Do random tests
-		System.out.println("Random Lookup Tests");
-		System.out.format("%-8s%-8s%s\n", "N", "Splay", "OptimalBST");
+		System.out.println("Random Lookup Tests with Sampling");
+		System.out.format("%-8s%-8s%s\n", "N", "SplayAvg", "OptimalBSTAvg");
 		for (int n = 1; n <= N; n++) {
-			splay = new Splay();
-			keys = new int[n];
-			freq = new int[n];
-			for (int number = 0; number < n; number++) {
-				keys[number] = number;
-				freq[number]++;
-				splay.insert(number);
-			}
-			
-			for (int lookup = 0; lookup < 100; lookup++) {
-				for (int i = 0; i < n; i++) {
-					int number = random.nextInt(n);
+			int splayAvg = 0;
+			int optimalBSTAvg = 0;
+			for (int sample = 0; sample < SAMPLES; sample++) {
+				splay = new Splay();
+				keys = new int[n];
+				freq = new int[n];
+				for (int number = 0; number < n; number++) {
+					keys[number] = number;
 					freq[number]++;
-					splay.find(number);
+					splay.insert(number);
 				}
+				
+				for (int lookup = 0; lookup < 100; lookup++) {
+					for (int i = 0; i < n; i++) {
+						int number = random.nextInt(n);
+						freq[number]++;
+						splay.find(number);
+					}
+				}
+				splayAvg += splay.getOperations();
+				optimalBSTAvg += OptimalBSTHelper.optimalSearchTree(keys, freq);
 			}
+			splayAvg /= SAMPLES;
+			optimalBSTAvg /= SAMPLES;
 			
-			System.out.format("%-8d%-8d%d\n", n, splay.getOperations(), OptimalBSTHelper.optimalSearchTree(keys, freq));
+			System.out.format("%-8d%-8d%d\n", n, splayAvg, optimalBSTAvg);
 		}
 		
 		System.out.println("Distribution of Lookups for N = " + keys.length);
